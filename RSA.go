9370e2ede35641da9ef1016e19f2e08e48b174f1
PimmyTrousers <pim.trouerbach1@gmail.com>
2016-07-07 19:05:04 -0700

    Works for input messages

--- RSA.go
+++ RSA.go
@@ -1,18 +1,91 @@
 package main
 
-import "fmt"
-
-func modInverse(e int, pn int) int {
-	e = e % pn
-	for d := 1; d < pn; d++ {
-		if (e*d)%pn == 1 {
-			return d
-		}
-	}
-	return 0
-}
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Enter 2 prime numbers and one number that is coprime: \n ")
+
+	// Generate RSA Keys
+	pimPrivateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	pimPublicKey := &pimPrivateKey.PublicKey
+
+	roshanPrivateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	roshanPublicKey := &roshanPrivateKey.PublicKey
+
+	fmt.Println("Pim Private Key : ", pimPrivateKey)
+	fmt.Println("Pim Public key ", pimPublicKey)
+	fmt.Println("Roshan Private Key : ", roshanPrivateKey)
+	fmt.Println("Roshan Public key ", roshanPublicKey)
+
+	//Encrypt pim Message
+	message := []byte("the code must be like a piece of music")
+	label := []byte("")
+	hash := sha256.New()
+
+	ciphertext, err := rsa.EncryptOAEP(hash, rand.Reader, roshanPublicKey, message, label)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("OAEP encrypted [%s] to \n[%x]\n", string(message), ciphertext)
+	fmt.Println()
+
+	// Message - Signature
+	var opts rsa.PSSOptions
+	opts.SaltLength = rsa.PSSSaltLengthAuto // for simple example
+	PSSmessage := message
+	newhash := crypto.SHA256
+	pssh := newhash.New()
+	pssh.Write(PSSmessage)
+	hashed := pssh.Sum(nil)
+
+	signature, err := rsa.SignPSS(rand.Reader, pimPrivateKey, newhash, hashed, &opts)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("PSS Signature : %x\n", signature)
+
+	// Decrypt Message
+	plainText, err := rsa.DecryptOAEP(hash, rand.Reader, roshanPrivateKey, ciphertext, label)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("OAEP decrypted [%x] to \n[%s]\n", ciphertext, plainText)
+
+	//Verify Signature
+	err = rsa.VerifyPSS(pimPublicKey, newhash, hashed, signature, &opts)
+
+	if err != nil {
+		fmt.Println("Who are U? Verify Signature failed")
+		os.Exit(1)
+	} else {
+		fmt.Println("Verify Signature successful")
+	}
 
 }
